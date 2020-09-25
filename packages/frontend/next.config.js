9a6ab5b5b96699f1f008d9307f3406dcaa416a26
Zachary Strangeway <zachary.strangeway@tasconline.com>
2020-09-24 20:22:31 -0500

    add env variables

---
+++ packages/frontend/next.config.js
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-undef */
+require("dotenv").config();
+
+module.exports = {
+  env: {
+    API_URL: process.env.API_URL,
+  },
+};
